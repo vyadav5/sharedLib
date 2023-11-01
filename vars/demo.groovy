4f9ecee6021e183dd7efe65bb726452e08fe9a20
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-01 20:36:35 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -3,17 +3,17 @@ def clone(String repoUrl) {
 }
 def approve() {
     timeout(time: 30, unit: 'MINUTES') {
-        // Request user approval with a custom message
+      
         def userInput = input(
             id: 'userInput',
             message: 'Do you want to proceed with the build?',
             parameters: [choice(name: 'APPROVE', choices: 'Approve\nReject', description: 'Choose approval status')]
         )
 
-        // Check if the user approved or rejected
+      
         if (userInput == 'Approve') {
             echo 'User approved the build.'
-            // Proceed with the build
+          
         } else {
             error('User rejected the build.')
         }
