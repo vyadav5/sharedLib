3734c0927f237a4e7d3840eacee812ef49071b1f
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-01 21:57:15 +0530

    Update and rename demo.groovy to demo.groovy

---
+++ vars/demo.groovy
@@ -0,0 +1,22 @@
+def clone(Map config) {
+    def repoUrl = config.REPO_URL
+    sh "git clone ${repoUrl}"
+}
+// def approve(Map config) {
+//     timeout(time: 30, unit: 'MINUTES') {
+      
+//         def userInput = input(
+//             id: 'userInput',
+//             message: 'Do you want to proceed with the build?',
+//             parameters: [choice(name: 'APPROVE', choices: 'Approve\nReject', description: 'Choose approval status')]
+//         )
+
+      
+//         if (userInput == 'Approve') {
+//             echo 'User approved the build.'
+          
+//         } else {
+//             error('User rejected the build.')
+//         }
+//     }
+// }
