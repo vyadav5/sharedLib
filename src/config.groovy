f854eeec1cbff9ec92e00c64cc9d8e5b8254a9b7
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-01 21:58:23 +0530

    Update and rename config.groovy to config.groovy

--- src/config.groovy
+++
@@ -1,6 +0,0 @@
-# Configuration
-KEEP_APPROVAL_STAGE=true
-SLACK_CHANNEL_NAME=build-status
-ENVIRONMENT=prod
-CODE_BASE_PATH=env/prod
-
---
+++ vars/config.groovy
@@ -0,0 +1,4 @@
+# Configuration
+config = [
+    REPO_URL: 'https://github.com/vyadav5/ansible-newRelic.git'
+]
