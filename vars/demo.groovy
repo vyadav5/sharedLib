24c5be07b280c9b85f3766ca3f9491d9a2248fe6
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-02 23:16:56 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -2,6 +2,17 @@ def clone(String repoUrl) {
     sh "git clone ${repoUrl}"
 }
 
+def call(Map params) {
+    // You can access the KEEP_APPROVAL_STAGE parameter from the Jenkinsfile
+    def approvalRequired = params.KEEP_APPROVAL_STAGE ?: "true"
+
+    if (approvalRequired == "true") {
+        echo "Approval stage is required."
+        // Add your approval stage logic here
+    } else {
+        echo "Skipping approval stage."
+    }
+}
 
 // def approve(Map config) {
 //     timeout(time: 30, unit: 'MINUTES') {
