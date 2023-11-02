5ec8951c2d4784add5449962d9ed0a05179a87d6
Vidhi Yadav <79339896+vyadav5@users.noreply.github.com>
2023-11-02 20:04:22 +0530

    Update demo.groovy

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,26 +1,8 @@
-// vars/demo.groovy
-def call() {
-    def config = libraryResource("resources/config.txt").read()
-
-    // Access and print the configuration settings
-    echo "SLACK_CHANNEL_NAME: ${config.SLACK_CHANNEL_NAME}"
-    echo "ENVIRONMENT: ${config.ENVIRONMENT}"
-    echo "CODE_BASE_PATH: ${config.CODE_BASE_PATH}"
-    echo "KEEP_APPROVAL_STAGE: ${config.KEEP_APPROVAL_STAGE}"
+def clone() {
+    sh "git clone ${repoUrl}"
 }
 
 
-// @Grab('org.json:json:20210307')
-// import groovy.json.JsonSlurper
-
-// def clone(Map config) {
-//     def configFile = readFile('resources/config.json')
-//     def parsedConfig = new JsonSlurper().parseText(configFile)
-
-
-//     def repoUrl = config.REPO_URL
-//     sh "git clone ${repoUrl}"
-// }
 // def approve(Map config) {
 //     timeout(time: 30, unit: 'MINUTES') {
       
